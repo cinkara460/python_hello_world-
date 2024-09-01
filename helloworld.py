c356e7454c6a8dcabf9e06b08380fbfeaa558672
cinkara <oguzkagancinkara@gmail.com>
2024-09-01 14:27:12 +0300

    added some prints

--- helloworld.py
+++ helloworld.py
@@ -5,4 +5,8 @@ Created on Sun Sep  1 14:20:37 2024
 @author: CINKARA
 """
 
-print("hello world");
+print("hello planet");
+print("cingara");
+print("basri");
+print("hello planet");
+print("hello planet");
